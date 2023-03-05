2e0dcf0a9e7948b1f0c5602f6af1ee9e69c47310
David <david.sernar@uqvirtual.edu.co>
2023-03-04 19:45:55 -0500

    Primera versión del proyecto

---
+++ src/modelo/Producto.java
@@ -0,0 +1,51 @@
+package modelo;
+
+public class Producto {
+
+    private String nombre;
+    private String descripcion;
+    private String codigo;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, String descripcion, String codigo) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", codigo='" + codigo + '\'' +
+                '}';
+    }
+    
+}
